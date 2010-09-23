2b93f8e6c8b377e7a06e1d7e6a011319943dd34f
David Gageot <gageot@algodeal.com>
2010-09-23 16:49:43 +0200

    Test for TopCoder sample

--- src/TothelloTest.java
+++ src/TothelloTest.java
@@ -1,9 +1,14 @@
-import org.fest.assertions.Fail;
-import org.junit.Test;
+import static org.fest.assertions.Assertions.*;
+import org.fest.util.*;
+import org.junit.*;
 
 public class TothelloTest {
 	@Test
-	public void canFail() {
-		Fail.fail("TODO");
+	public void canPassTopCoderSample() {
+		assertThat(bestMove(Arrays.array("A1", "B8", "C6", "C8", "D8"), Arrays.array("B2", "C2", "C3", "C4", "C5"), "Red")).isEqualTo(11);
+	}
+
+	private int bestMove(String[] redPieces, String[] blackPieces, String whoseTurn) {
+		return 0;
 	}
 }
